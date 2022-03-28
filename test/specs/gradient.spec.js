e1c52f998c294aec0ee74c95161bb5c3f3c711e6
stockiNail <stocki.nail@gmail.com>
2022-03-28 10:41:33 +0200

    Add test case for missing axis

--- test/specs/gradient.spec.js
+++ test/specs/gradient.spec.js
@@ -2,4 +2,38 @@ describe('Gradient plugin', function() {
 
   describe('auto', jasmine.fixtures('.'));
 
+  it('should emit console warning when unknown axis type is used', function() {
+    const origWarn = console.warn;
+    console.warn = jasmine.createSpy('warn');
+
+    acquireChart({
+      type: 'bar',
+      data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+        datasets: [{
+          data: [65, 29, 80, 91, 55, 5, 40],
+          gradient: {
+            backgroundColor: {
+              axis: 's',
+              colors: {
+                Jan: 'red',
+                Apr: 'yellow',
+                Jun: 'green',
+              }
+            }
+          }
+        }],
+      }
+    });
+
+    expect(console.warn).toHaveBeenCalledWith({
+      asymmetricMatch: function(compareTo) {
+        return compareTo.startsWith('Scale not found for \'s\'-axis in datasets[0] of chart id');
+      }
+    });
+
+    console.warn = origWarn;
+  });
+
+
 });
