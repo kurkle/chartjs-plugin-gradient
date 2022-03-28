73614b4c27fc2f50b215d310c571d88fb3cff8c8
stockiNail <stocki.nail@gmail.com>
2022-03-28 16:29:08 +0200

    Enable gradient styles on the legend items

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import {Chart} from 'chart.js';
-import {isNumber, color} from 'chart.js/helpers';
+import {isNumber, color, defined, valueOrDefault} from 'chart.js/helpers';
+
+const chartStates = new Map();
+const legendOptions = [
+  {key: 'backgroundColor', legendItemKey: 'fillStyle'},
+  {key: 'borderColor', legendItemKey: 'strokeStyle'}];
 
 function createGradient(ctx, axis, scale) {
   if (axis === 'r') {
@@ -31,15 +36,24 @@ function getPixelStop(scale, value) {
   return {pixel, stop: reverse ? 1 - stop : stop};
 }
 
-function addColors(gradient, scale, colors) {
+function applyColors(gradient, colors) {
+  colors.forEach(function(item) {
+    gradient.addColorStop(
+      item.stop, item.colorRgbString
+    );
+  });
+}
+
+function addColors(scale, colors, stopColors) {
   for (const value of Object.keys(colors)) {
     const {pixel, stop} = getPixelStop(scale, value);
     if (isFinite(pixel) && isFinite(stop)) {
       const colorStop = color(colors[value]);
       if (colorStop && colorStop.valid) {
-        gradient.addColorStop(
-          Math.max(0, Math.min(1, stop)), colorStop.rgbString()
-        );
+        stopColors.push({
+          stop: Math.max(0, Math.min(1, stop)),
+          colorRgbString: colorStop.rgbString()
+        });
       }
     }
   }
@@ -58,19 +72,62 @@ function setValue(meta, dataset, key, value) {
   }
 }
 
+function setLegendItem(chart, keyOption, item, area) {
+  const state = chartStates.get(chart);
+  const ctx = chart.ctx;
+  if (state.options.has(keyOption.key)) {
+    const option = state.options.get(keyOption.key);
+    const gradientData = option.filter((el) => el.datasetIndex === item.datasetIndex);
+    if (gradientData.length === 1) {
+      const data = gradientData[0];
+      const value = createGradient(ctx, data.axis, area);
+      applyColors(value, data.stopColors);
+      item[keyOption.legendItemKey] = value;
+    }
+  }
+}
+
+function getStateOptions(state, meta, key, datasetIndex) {
+  let stateOptions = state.options.get(key);
+  if (!stateOptions) {
+    stateOptions = [];
+    state.options.set(key, stateOptions);
+  } else if (!meta.hidden) {
+    stateOptions = stateOptions.filter((el) => el.datasetIndex !== datasetIndex);
+    state.options.set(key, stateOptions);
+  }
+  return stateOptions;
+}
+
 const getScale = Chart.version
   ? (meta, axis) => meta[axis + 'Scale']
   : (meta, axis) => meta.controller['_' + axis + 'Scale'];
 
 const areaIsValid = (area) => area && area.right > area.left && area.bottom > area.top;
 
+const legendFontSize = (options) => options.plugins.legend.labels.font && defined(options.plugins.legend.labels.font.size)
+  ? options.plugins.legend.labels.font.size
+  : options.font.size;
+
+const legendBoxHeight = Chart.version
+  ? (options) => legendFontSize(options)
+  : (options) => valueOrDefault(options.legend.labels.fontSize, options.defaultFontSize);
+
 export default {
   id: 'gradient',
+
+  beforeInit(chart) {
+    const state = new Object();
+    state.options = new Map();
+    chartStates.set(chart, state);
+  },
+
   beforeDatasetsUpdate(chart) {
     const area = chart.chartArea;
     if (!areaIsValid(area)) {
       return;
     }
+    const state = chartStates.get(chart);
     const ctx = chart.ctx;
     const datasets = chart.data.datasets;
     for (let i = 0; i < datasets.length; i++) {
@@ -88,12 +145,49 @@ export default {
           console.warn(`Scale not found for '${axis}'-axis in datasets[${i}] of chart id ${chart.id}, skipping.`);
           continue;
         }
-        if (colors) {
+        const stateOptions = getStateOptions(state, meta, key, i);
+        if (colors && !meta.hidden) {
+          const option = {
+            datasetIndex: i,
+            axis,
+            stopColors: []
+          };
+          stateOptions.push(option);
           const value = createGradient(ctx, axis, scale);
-          addColors(value, scale, colors);
+          addColors(scale, colors, option.stopColors);
+          applyColors(value, option.stopColors);
           setValue(meta, dataset, key, value);
         }
       }
     }
+  },
+
+  afterUpdate(chart) {
+    if (chart.legend) {
+      const boxHeight = chart.legend.options.labels.boxHeight
+        ? chart.legend.options.labels.boxHeight
+        : legendBoxHeight(chart.options);
+      const boxWidth = chart.legend.options.labels.boxWidth;
+      const datasets = chart.data.datasets;
+      for (let i = 0; i < datasets.length; i++) {
+        const item = chart.legend.legendItems[i];
+        const hitBox = chart.legend.legendHitBoxes[i];
+        const area = {
+          top: hitBox.top,
+          left: hitBox.left,
+          bottom: hitBox.top + boxHeight,
+          right: hitBox.left + boxWidth,
+          xCenter: hitBox.left + boxWidth / 2,
+          yCenter: hitBox.top + boxHeight / 2,
+          drawingArea: Math.max(boxWidth, boxHeight) / 2
+        };
+        if (areaIsValid(area)) {
+          legendOptions.forEach(function(keyOption) {
+            setLegendItem(chart, keyOption, item, area);
+          });
+        }
+      }
+    }
   }
+
 };
